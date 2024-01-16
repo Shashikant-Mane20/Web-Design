345a6fdf0df6901ccbeb8610ead79465608d4a42
Shashikant-Mane20 <shashikant.mane801@gmail.com>
2024-01-16 20:44:37 +0530

    Javascript

---
+++ JS/01_Datatypes/script.js
@@ -0,0 +1,14 @@
+console.log(typeof 8)
+console.log(typeof 8.9)
+console.log(typeof "Hello")
+console.log(typeof '200')
+console.log(typeof +'200') //string to number
+console.log(typeof +'shashi') //not a number
+console.log(typeof 100 + '') //number to string
+console.log(typeof true)
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null)
+console.log(typeof $); //undefined
+console.log(`My name is Shashikant
+and my age is 20 years.`)  //multiline strings using backticks
---
+++ JS/02_variables_in_javascript/script.js
@@ -0,0 +1,43 @@
+'use strict';
+console.log(myName) //===> It will give me undefined 
+
+//console.log(firstName) //====> It will give me error cannot access
+
+//let
+let firstName="Shashikant";
+
+firstName= 'Yash'
+
+let lastName="Mane";
+
+let age=20;
+
+let userIntro='My name is '+firstName+' '+lastName+' and I am '+age+' years old.';
+
+let isUnhappy= false;
+
+let isHappy= true;
+
+console.log(userIntro);
+
+let a=null
+
+let b  //undefined
+
+//const
+
+const hrsInDay=24
+
+// hrsInDay=25 we cannot re declare it 
+
+//var
+
+var myName='Shashikant'
+
+myName='Yash'
+var surname='Mane'
+
+console.log('My name is '+myName+' '+surname);
+
+//let var diff
+//let const diff
